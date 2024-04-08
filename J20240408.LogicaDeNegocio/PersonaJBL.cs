3e815128d5906a5e48702282b14abbe4bb538ddd
Diego Edenilson Martinez Lopez <ml24002@esfe.agape.edu.sv>
2024-04-08 09:37:38 -0600

    se crearon las clases DependencyContainer y PersonJBL en LogicaDeNegocio

---
+++ J20240408.LogicaDeNegocio/PersonaJBL.cs
@@ -0,0 +1,41 @@
+﻿using J20240408.AccesoADatos;
+using J20240408.EntidadesDeNegocio;
+
+namespace J20240408.LogicaDeNegocio
+{
+    public class PersonaJBL
+    {
+        readonly PersonaJDAL _personaJDAL;
+        public PersonaJBL(PersonaJDAL personaJDAL)
+        {
+            _personaJDAL = personaJDAL;
+        }
+        public async Task<int> Crear(PersonaJ personasJ)
+        {
+
+            return await _personaJDAL.Crear(personasJ);
+        }
+        public async Task<int> Modificar(PersonaJ personaJ)
+        {
+
+            return await _personaJDAL.Modificar(personaJ);
+        }
+        public async Task<int> Eliminar(PersonaJ personasJ)
+        {
+
+            return await _personaJDAL.Eliminar(personasJ);
+        }
+        public async Task<PersonaJ> ObtenerPorId(PersonaJ personasJ)
+        {
+            return await _personaJDAL.ObtenerPorId(personasJ);
+        }
+        public async Task<List<PersonaJ>> ObtenerPorTodos(PersonaJ personasJ)
+        {
+            return await _personaJDAL.ObtenerPorTodos(personasJ);
+        }
+        public async Task<List<PersonaJ>> Buscar(PersonaJ personasJ)
+        {
+            return await _personaJDAL.Buscar(personasJ);
+        }
+    }
+}
